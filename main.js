22dc9606c3408f18581546a37378e268b1617762
NotTheWhiteRanger <tsukitheotaku@gmail.com>
2025-04-02 17:15:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ import {
   onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyD2B6KZgtYQPE4K-JF5GQszp5wjNgX6_MY",
   authDomain: "new-chat-8d4f4.firebaseapp.com",
