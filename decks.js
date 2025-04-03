a419100bdc5907523b8d4b4e3c27533f7faea4f4
NotTheWhiteRanger <tsukitheotaku@gmail.com>
2025-04-02 23:42:20 -0500

    Create decks.js

---
+++ decks.js
@@ -0,0 +1,51 @@
+// decks.js
+export const commanderDecks = [
+  {
+    id: 'deck1',
+    name: "Aggressive Assault",
+    commander: "Kaalia of the Vast",
+    description: "A fast-paced, aggressive deck with an arsenal of creatures.",
+    deckList: [
+      "Gisela, Blade of Goldnight",
+      "Anger",
+      "Balefire Dragon",
+      // ...more cards
+    ]
+  },
+  {
+    id: 'deck2',
+    name: "Control & Conquer",
+    commander: "Oloro, Ageless Ascetic",
+    description: "A deck built around control elements and life gain.",
+    deckList: [
+      "Swords to Plowshares",
+      "Counterspell",
+      "Teferi, Hero of Dominaria",
+      // ...more cards
+    ]
+  },
+  {
+    id: 'deck3',
+    name: "Ramp and Dominate",
+    commander: "The Ur-Dragon",
+    description: "A deck focused on ramping mana and unleashing big dragons.",
+    deckList: [
+      "Cultivate",
+      "Kodama's Reach",
+      "Dragonlord Atarka",
+      // ...more cards
+    ]
+  },
+  {
+    id: 'deck4',
+    name: "Token Army",
+    commander: "Edgar Markov",
+    description: "A swarm strategy deck that builds on tokens and synergy.",
+    deckList: [
+      "Lightning Mauler",
+      "Gather the Townsfolk",
+      "Throne of the God-Pharaoh",
+      // ...more cards
+    ]
+  },
+];
